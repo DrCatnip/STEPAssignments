9e0e1d2cf676a35aeba7573b3b255efea4b58e01
DrCatnip <masterpanshul.164@gmail.com>
2026-03-06 19:34:12 +0530

    Finished level 3 problem 5

---
+++ Level3_5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Level3_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+
+        System.out.println(
+            "The swapped numbers are " + number1 + " and " + number2
+        );
+
+        sc.close();
+    }
+}
