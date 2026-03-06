bf90422dfe5d1d7ed0c4c2a4bcf813af959edb36
DrCatnip <masterpanshul.164@gmail.com>
2026-03-06 20:04:36 +0530

    Finished level 2 problem 3

---
+++ Level2_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Level2_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        sc.close();
+    }
+}
