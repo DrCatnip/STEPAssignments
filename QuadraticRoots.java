be140ece28d5d12b1ea1d518ca906242739bdab8
DrCatnip <masterpanshul.164@gmail.com>
2026-04-11 11:46:38 +0530

    finished level 2 problem 11

---
+++ QuadraticRoots.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class QuadraticRoots {
+
+    public double[] findRoots(double a, double b, double c) {
+
+        double delta = Math.pow(b, 2) - 4 * a * c;
+
+        if (delta > 0) {
+            double r1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double r2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[]{r1, r2};
+        }
+        else if (delta == 0) {
+            double r = -b / (2 * a);
+            return new double[]{r};
+        }
+
+        return new double[0];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a, b, c: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        QuadraticRoots obj = new QuadraticRoots();
+
+        double[] roots = obj.findRoots(a, b, c);
+
+        if (roots.length == 0)
+            System.out.println("No real roots");
+        else {
+            for (double r : roots)
+                System.out.println("Root = " + r);
+        }
+
+        sc.close();
+    }
+}
