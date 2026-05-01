3411f8a19387e3e38ef308b01246cb727890b3ff
DrCatnip <masterpanshul.164@gmail.com>
2026-05-01 14:46:01 +0530

    Finished level 3 problem 1

---
+++ BMITracker.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class BMITracker {
+
+    // Calculate BMI and status
+    public String[][] calculate(double[][] data) {
+
+        String[][] result = new String[data.length][4];
+
+        for (int i = 0; i < data.length; i++) {
+
+            double weight = data[i][0];
+            double heightM = data[i][1] / 100;
+
+            double bmi = weight / (heightM * heightM);
+
+            String status;
+
+            if (bmi < 18.5) status = "Underweight";
+            else if (bmi < 25) status = "Normal";
+            else if (bmi < 30) status = "Overweight";
+            else status = "Obese";
+
+            result[i][0] = String.valueOf(weight);
+            result[i][1] = String.valueOf(data[i][1]);
+            result[i][2] = String.format("%.2f", bmi);
+            result[i][3] = status;
+        }
+
+        return result;
+    }
+
+    public void display(String[][] arr) {
+
+        System.out.println("Weight\tHeight\tBMI\tStatus");
+
+        for (String[] row : arr)
+            System.out.println(row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + row[3]);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double[][] data = new double[3][2];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter weight and height: ");
+            data[i][0] = sc.nextDouble();
+            data[i][1] = sc.nextDouble();
+        }
+
+        BMITracker obj = new BMITracker();
+        obj.display(obj.calculate(data));
+
+        sc.close();
+    }
+}
